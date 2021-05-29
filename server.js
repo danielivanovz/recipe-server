826f0dd091a510184496d5dbca3198f69b6b420c
Daniel <daniel.ivanov@me.com>
2021-05-29 19:34:28 +0200

    cors/ingredient endpoint

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const { MongoClient } = require('mongodb');
 
-//app.use(express.static('public'));
-
-app.get('/recipe', async function (req, res) {
+app.get('/:ingredient', cors(), async (req, res) => {
+	console.log(req.params.ingredient);
 	const client = new MongoClient(process.env.URI, { useUnifiedTopology: true });
 
 	try {
 		await client.connect();
 
 		const collection = client.db(process.env.DB).collection(process.env.COL);
-
-		const query = { 'ingredients.ingredient': 'Uova' };
+		const query = { 'ingredients.ingredient': `${req.params.ingredient}` };
 		const cursor = collection.aggregate([{ $match: query }]);
-
 		const recipe = await cursor.limit(20).toArray();
 
 		return res.json(recipe);
